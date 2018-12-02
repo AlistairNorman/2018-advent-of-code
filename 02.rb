44b863f3ab36504494df0991ed87130c5905fe45
Alistair Norman <alistair@super.gd>
2018-12-01 23:08:57 -0800

    02 part 1

--- 02.rb
+++ 02.rb
@@ -1,6 +1,27 @@
 #!/usr/bin/env ruby
 require 'pry'
 
-data = File.read("data/02.txt").lines
+data = File.read("data/02.txt").lines.map(&:strip)
 
-binding.pry
+doubles = 0
+triples = 0
+
+data.each do |datum|
+  datum = datum.split('')
+  seen = Hash.new
+
+  datum.each do |char|
+    seen[char] ||= 0
+    seen[char] += 1
+  end
+
+  if seen.find{|key,value| value == 3}
+    triples += 1
+  end
+
+  if seen.find{|key,value| value == 2}
+    doubles += 1
+  end
+end
+
+puts doubles * triples
