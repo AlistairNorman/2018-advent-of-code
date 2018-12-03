2a96459610be9a6bfeb0f456c964f808102ae90d
Alistair Norman <alistair@super.gd>
2018-12-02 21:54:01 -0800

    03 part 2

--- 03.rb
+++ 03.rb
@@ -36,3 +36,47 @@ taken.each do |took|
   end
 end
 puts count
+
+def claim(datum)
+  claim = []
+  x = datum.first
+  i = 0
+  while i < datum[2] do
+    y = datum[1]
+    j = 0
+    while j < datum.last do
+      claim << [x+i, y+j]
+      j += 1
+    end
+    i += 1
+  end
+  claim
+end
+
+def intersecting?(claim1, claim2)
+  if (claim1 + claim2).uniq.count == (claim1.count + claim2.count) || claim1 == claim2
+    return false
+  end
+  return true
+end
+
+
+claims = []
+data.each do |datum|
+  claims << claim(datum)
+end
+
+iterations = 0
+claimscount = 0
+claims.each do |claim1|
+  intersecting = false
+  claims.each do |claim2|
+    if intersecting?(claim1, claim2)
+      intersecting =  true
+      break
+    end
+  end
+  unless intersecting
+    puts claim1
+  end
+end
